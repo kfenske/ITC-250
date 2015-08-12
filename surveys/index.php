97f1b91024a950c83c7434a0fc5c475e76c3fc9a
kfenske <kayla.fenske@gmail.com>
2015-08-12 13:30:14 -0700

    Adds the response list/view and updates the survey list/view

--- surveys/index.php
+++ surveys/index.php
@@ -25,7 +25,8 @@ require '../inc_0700/config_inc.php'; #provides configuration, pathing, error ha
  
 # SQL statement
 //$sql = "select MuffinName, MuffinID, Price from test_Muffins";
-$sql = "select Title, Description, SurveyID from sm15_surveys";
+$sql = "select CONCAT(a.FirstName, ' ', a.LastName) AdminName, s.SurveyID, s.Title, s.Description, 
+date_format(s.DateAdded, '%W %D %M %Y %H:%i') 'DateAdded' from ". PREFIX . "surveys s, " . PREFIX . "Admin a where s.AdminID=a.AdminID order by s.DateAdded desc";
 
 #Fills <title> tag. If left empty will default to $PageTitle in config_inc.php  
 $config->titleTag = 'Muffins made with love & PHP in Seattle';
@@ -53,32 +54,45 @@ get_header(); #defaults to theme header or header_inc.php
 ?>
 <h3 align="center"><?=smartTitle();?></h3>
 
-<p>This page, along with <b>demo_view_pager.php</b>, demonstrate a List/View web application.</p>
-<p>It was built on the mysql shared web application page, <b>demo_shared.php</b></p>
-<p>This page is the entry point of the application, meaning this page gets a link on your web site.  Since the current subject is muffins, we could name the link something clever like <a href="<?php echo VIRTUAL_PATH; ?>demo_list_pager.php">Muffins</a></p>
-<p>Use <b>demo_list_pager.php</b> and <b>demo_view_pager.php</b> as a starting point for building your own List/View web application!</p> 
 <?php
 #reference images for pager
 $prev = '<img src="' . VIRTUAL_PATH . 'images/arrow_prev.gif" border="0" />';
 $next = '<img src="' . VIRTUAL_PATH . 'images/arrow_next.gif" border="0" />';
 
 # Create instance of new 'pager' class
-$myPager = new Pager(10,'',$prev,$next,'');
-$sql = $myPager->loadSQL($sql);  #load SQL, add offset
+#$myPager = new Pager(10,'',$prev,$next,'');
+#$sql = $myPager->loadSQL($sql);  #load SQL, add offset
 
 # connection comes first in mysqli (improved) function
 $result = mysqli_query(IDB::conn(),$sql) or die(trigger_error(mysqli_error(IDB::conn()), E_USER_ERROR));
 
 if(mysqli_num_rows($result) > 0)
 {#records exist - process
-	if($myPager->showTotal()==1){$itemz = "survey";}else{$itemz = "surveys";}  //deal with plural
-    echo '<div align="center">We have ' . $myPager->showTotal() . ' ' . $itemz . '!</div>';
-	while($row = mysqli_fetch_assoc($result))
-	{# process each row
-         echo '<div align="center"><a href="' . VIRTUAL_PATH . 'surveys/survey_view.php?id=' . (int)$row['SurveyID'] . '">' . dbOut($row['Title']) . '</a>';
-         echo '</div>';
+	#if($myPager->showTotal()==1){$itemz = "survey";}else{$itemz = "surveys";}  //deal with plural
+    #echo '<div align="center">We have ' . $myPager->showTotal() . ' ' . $itemz . '!</div>';
+    echo '<table align="center" border="1" style="border-collapse:collapse" cellpadding="3" cellspacing="3">';
+	echo '<tr>
+			<th>Date Added</th>
+			<th>Title</th>
+			<th>Description</th>
+			<th>Creator\'s Name</th>
+		</tr>
+		';
+		while($row = mysqli_fetch_assoc($result))
+		{# process each row
+			echo '<tr>
+					<td>' . dbOut($row['DateAdded']) . '</td>
+				    <td><a href="' . VIRTUAL_PATH . 'surveys/survey_view.php?id=' . (int)$row['SurveyID'] . '">' . dbOut($row['Title']) . '</a></td>
+				    <td>' . dbOut($row['Description']) . '</td>
+				    <td>' . dbOut($row['AdminName']) . '</td>
+				</tr>
+				';
+		echo '</table>';
+
+         #echo '<div align="center"><a href="' . VIRTUAL_PATH . 'surveys/survey_view.php?id=' . (int)$row['SurveyID'] . '">' . dbOut($row['Title']) . '</a>';
+         #echo '</div>';
 	}
-	echo $myPager->showNAV(); # show paging nav, only if enough records	 
+	#echo $myPager->showNAV(); # show paging nav, only if enough records	 
 }else{#no records
     echo "<div align=center>There are currently no surveys.</div>";	
 }
